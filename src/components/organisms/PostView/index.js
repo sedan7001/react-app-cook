4ee60069b05fef217bc22356cacf9e65f0731a08
sedan <sedan7001@gmail.com>
2018-07-23 16:36:07 +0900

    에러수정. eject하기 전.

--- src/components/organisms/PostView/index.js
+++ src/components/organisms/PostView/index.js
@@ -22,11 +22,11 @@ const PostView = () => {
             <div className="ripple">
                 <form name="ripple_form" method="post" action="insert_ripple.php?table=cook_free&amp;num=21">
                     <div className="ripple_box">
-                        <div className="ripple_box1"><img src={titleComment}/></div>
+                        <div className="ripple_box1"><img src={titleComment} alt="titleComment"/></div>
                         <div className="ripple_box2"><textarea rows="5" cols="65" name="ripple_content"></textarea>
                         </div>
-                        <div className="ripple_box3"><a href="#"><img src={commentSubmitBtn}
-                                                                      onClick="check_input()"/></a></div>
+                        <div className="ripple_box3"><a><img src={commentSubmitBtn}
+                                                             onClick="check_input()" alt="commentSubmitBtn"/></a></div>
                     </div>
                 </form>
             </div>
@@ -34,7 +34,7 @@ const PostView = () => {
                 <button className="smallButton"><a href="list.php?table=cook_free&amp;page=1">목록</a></button>
                 <button className="smallButton"><a
                     href="write_form.php?table=cook_free&amp;mode=modify&amp;num=21&amp;page=1">수정</a></button>
-                <button className="smallButton"><a href="javascript:del('delete.php?table=cook_free&amp;num=21')">삭제</a>
+                <button className="smallButton"><a>삭제</a>
                 </button>
                 <button className="writeButton"><a href="write_form.php?table=cook_free">글쓰기</a></button>
             </div>
