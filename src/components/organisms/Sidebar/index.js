af0016e5ca137ecf70d69cd95ed90d8437db5f3c
sedan <sedan7001@gmail.com>
2018-08-31 03:43:20 +0900

    sidebar에 NavLink 수정.

--- src/components/organisms/Sidebar/index.js
+++ src/components/organisms/Sidebar/index.js
@@ -9,12 +9,12 @@ const Sidebar = () => {
                 <span>메뉴</span>
             </div>
             <ul>
-                <li><NavLink to="/about" className="linkToPage"> ABOUT </NavLink></li>
-                <li><NavLink to="/greet" className="linkToPage"> 가입인사 </NavLink></li>
-                <li><NavLink to="/memo" className="linkToPage"> 낙서장 </NavLink></li>
-                <li><NavLink to="/free" className="linkToPage"> 자유게시판 </NavLink></li>
-                <li><NavLink to="/download" className="linkToPage"> 자료실 </NavLink></li>
-                <li><NavLink to="/qna" className="linkToPage"> 참여문의 </NavLink></li>
+                <li><NavLink to="/about/post/6" className="linkToPage"> ABOUT </NavLink></li>
+                <li><NavLink to="/greet/page/1" className="linkToPage"> 가입인사 </NavLink></li>
+                <li><NavLink to="/memo/page/1" className="linkToPage"> 낙서장 </NavLink></li>
+                <li><NavLink to="/free/page/1" className="linkToPage"> 자유게시판 </NavLink></li>
+                <li><NavLink to="/download/page/1" className="linkToPage"> 자료실 </NavLink></li>
+                <li><NavLink to="/qna/page/1" className="linkToPage"> 참여문의 </NavLink></li>
                 <li><NavLink to="/survey" className="linkToPage"> 설문조사 </NavLink></li>
             </ul>
         </div>
